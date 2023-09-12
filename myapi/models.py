339fa4572575d4a4dd5685efc16c570e29e9249a
dtgamer <144635041+dtgamer@users.noreply.github.com>
2023-09-12 11:39:48 +0100

    Update models.py

--- myapi/models.py
+++ myapi/models.py
@@ -1,7 +1,4 @@
 from django.db import models
 
 class Person(models.Model):
-    name = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.name
+    name = models.CharField(max_length=255, unique=True)
