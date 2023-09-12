e4e7a657310f28fb3b3a1f9af8ac56bd20eb4ee6
dtgamer <144635041+dtgamer@users.noreply.github.com>
2023-09-12 15:52:10 +0100

    Update urls.py

--- myapi/urls.py
+++ myapi/urls.py
@@ -3,5 +3,5 @@ from .views import PersonCreateView, PersonRetrieveUpdateDeleteView
 
 urlpatterns = [
     path('api/', PersonCreateView.as_view(), name='person-create'),  # CREATE: Adding a new person
-    path('api/<str:name>/', PersonRetrieveUpdateDeleteView.as_view(), name='person-retrieve-update-delete'),  # READ, UPDATE, DELETE
+    path('api/<int=pk>', PersonRetrieveUpdateDeleteView.as_view(), name='person-retrieve-update-delete'),  # READ, UPDATE, DELETE
 ]
