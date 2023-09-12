f28489695725aa513766ebf4848d1e2eee1b487c
dtgamer <144635041+dtgamer@users.noreply.github.com>
2023-09-12 11:38:00 +0100

    Update serializers.py

--- myapi/serializers.py
+++ myapi/serializers.py
@@ -4,9 +4,4 @@ from .models import Person
 class PersonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Person
-        fields = ('name',)
-
-    def validate_name(self, value):
-        if not value.strip():
-            raise serializers.ValidationError("Name cannot be empty.")
-        return value
+        fields = '__all__'
